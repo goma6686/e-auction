911e9f7fd6c1e970b196f32090267344bf67cbd9
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-09-25 11:25:59 +0300

    design start

---
+++ resources/views/components/categorycard.blade.php
@@ -0,0 +1,10 @@
+<div class="col" id="categories">
+    <div class="col">
+        <div class="card" style="width: 10rem;">
+            <img src="#" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title">CATEGORY</h5>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/topbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand">
+        <i class="fa fa-superpowers" aria-hidden="true"></i> <!-- Icon -->
+            GoAuction
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+        </div>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
