a9c6dc93830d050ae93d68ed9217cae425383a65
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-11-22 20:51:37 +0200

    item card adjustments

--- resources/views/components/itemcard.blade.php
+++ resources/views/components/itemcard.blade.php
@@ -3,7 +3,7 @@
         <div class="card-img-overlay h-50 text-end">
             @include('components.heart')    
         </div>
-        <img id="item-image" @if($auction->items[0]->image != null) src="/images/{{ $auction->items[0]->image }}" @else src="/images/noimage.jpg" @endif class="card-img-top" alt="{{$auction->title}}">
+        <img id="item-image" @if($auction->items[0]->image != null) src="/images/{{ $auction->items[0]->image }}" @else src="/images/noimage.jpg" @endif class="card-img-top mx-auto d-block" alt="{{$auction->title}}">
     </div>
     
     <div class="card-body text-center">
@@ -12,36 +12,40 @@
                 <h5 class="card-title">{{$auction->title}}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{{$auction->category->category}}</h6>
             </li>
-            <li class="list-group-item" id="price-bids">
+            <li class="list-group-item">
                 <div class="row">
-                    <div class="col-md-6 text-center">
-                        @if ($auction->count == 1)
-                            <h6 class="card-subtitle mb-2">Current price:</h6>
+                    <div class="col">
+                        @if ($auction->price != null)
+                            <h6 class="card-subtitle mb-2">Price:</h6>
                             <h6 class="card-subtitle mb-2">{{$auction->price}} Eur</h6>
                         @else
                             <h6 class="card-subtitle mb-2">From:</h6>
                             <h6 class="card-subtitle mb-2">{{$auction->min_price}} Eur</h6>
                         @endif
                     </div>
-                    <div class="col-md-6 text-center">
-                        <h6 class="card-subtitle mb-2">Current bids:</h6>
-                        <h6 class="card-subtitle mb-2">{{$auction->bidder_count}}</h6>
-                    </div>
+                    @if ($auction->type_id === '2')
+                        <div class="col-md-6 text-center">
+                            <h6 class="card-subtitle mb-2">Current bids:</h6>
+                            <h6 class="card-subtitle mb-2">{{$auction->bidder_count}}</h6>
+                        </div>
+                    @endif
                 </div>
             </li>
           </ul>
     </div>
     <div class="card-footer text-muted">
-        <div class="row">
-            <div class="col text-center">
-                Ends at:
+        @if ($auction->end_time != null)
+            <div class="row">
+                <div class="col text-center">
+                    Ends at:
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col text-center">
-                {{$auction->end_time}}
+            <div class="row">
+                <div class="col text-center">
+                    {{$auction->end_time}}
+                </div>
             </div>
-        </div>
+        @endif
         <div class="d-grid gap-2 col-3 mx-auto">
             <a class="btn btn-dark" role="button" href="/auction/{{ $auction->uuid }}">BID</a>
         </div> 
