0bb5e3e23c20bede53fe0170685c597a51680a13
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-11-22 20:52:10 +0200

    + quantity to items

--- resources/views/livewire/choose-item.blade.php
+++ resources/views/livewire/choose-item.blade.php
@@ -18,9 +18,18 @@
     <h5 class="row">
         <div class="col-3">Condition:</div>
         <div class="col-7 text-start">{{ $condition }}</div>
-      </h5>
-      <h5 class="row">
-        <div class="col-3">Current Price, €:</div>
+    </h5>
+    <h5 class="row">
+        <div class="col-3">Price, €:</div>
         <div class="col-7 text-start" id="price">{{ $price }}</div>
     </h5>
+    @if ($auction->type_id === '1')
+        <h5 class="row">
+            <div class="col-3">Quantity:</div>
+            <div class="col-7">
+              <input id="quantity"  type="number" name="quantity" placeholder="1" step="1" min="1">
+              <h6>Left: {{ $quantity }}</h6>
+            </div>
+          </h5>
+    @endif
 </div>
