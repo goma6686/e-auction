b32182d1519fe2f28be44a868e97ac7aa4836573
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-11-22 20:52:00 +0200

    better create auction

--- resources/views/livewire/create-auction.blade.php
+++ resources/views/livewire/create-auction.blade.php
@@ -1,17 +1,13 @@
 <div>
-    <form enctype="multipart/form-data" method="POST" action="{{ route('store-auction') }}">
+    <form enctype="multipart/form-data" method="POST" action="{{ route('store-auction', ['type' => $type]) }}">
         @csrf
         <div class="form-group">
             <label for="title">Auction title*</label>
             <input type="text" name="title" wire:model="title" class="form-control @error('title') is-invalid @enderror" required>
         </div>
         <div class="form-group pt-4">
-            <label for="description">Auction description*</label>
+            <label for="description">Auction description* </label>
             <textarea wire:model="description" name="description" type="text" rows="5" class="form-control @error('description') is-invalid @enderror"></textarea>
-        </div>
-        <div class="form-group pt-4">
-            <label for="end_time">Enter auction end date and time*: (after {{ \Carbon\Carbon::now()->toDateString() }})</label><br>
-            <input wire:model="end_time" type="datetime-local" name="end_time" class=" @error('end_time') is-invalid @enderror" required>
         </div>
         <div class="form-group pt-4">
             <div class="col-md-2">
@@ -26,62 +22,120 @@
             </select>
             </div>
         </div>
-        <div class="form-check pt-4">
-            <label for="is_active">Active?</label>
-            <input class="form-check-input" type="checkbox" value="0" name="is_active" wire:model="is_active">
+        @if ($type === '2')
+            <div class="form-group pt-4">
+                <label for="start_time">Start date and time*: (after {{ \Carbon\Carbon::now()->toDateString() }})</label><br>
+                <input wire:model="start_time" type="datetime-local" name="start_time" class=" @error('start_time') is-invalid @enderror" required>
+            </div>
+            <div class="form-group pt-4">
+                <label for="end_time">End date and time*: (after {{ \Carbon\Carbon::now()->toDateString() }})</label><br>
+                <input wire:model="end_time" type="datetime-local" name="end_time" class=" @error('end_time') is-invalid @enderror" required>
+            </div>
+        @endif
+        <div class="form-group pt-4">
+            <div class="col-md-2">
+                <label for="is_active">Status</label>
+                <select class="form-control" name="is_active" wire:model="is_active">
+                    <option value="0" selected>Inactive</option>
+                    <option value="1">Active</option>
+                </select>
+            </div>
         </div>
 
-        <div class="card">
-            <div class="card-body">
-                @foreach ($items as $index => $item)
-                    <hr>
-                    <div class="form-group">
-                        <label for="item_title">Item title</label>
-                        <input type="text"
-                                name="items[{{$index}}][item_title]"
-                                class="form-control"
-                                wire:model="items.{{$index}}.item_title" />
-                    </div>
-                    <div class="form-group pt-4">
-                        <input type="file"
-                                name="items[{{$index}}][image]"
-                                placeholder="Choose image"
-                                id="image"
-                                wire:model="items.{{$index}}.image" />
-                    </div>
-                    <div class="form-group pt-4">
-                        <div class="col-md-2">
-                            <label>Condition*</label>
-                            <select class="form-control"
-                                    name="items[{{$index}}][condition]"
-                                    wire:model="items.{{$index}}.condition"
-                                    required>
-                                    @foreach($conditions as $condition)
-                                        <option value="{{ $condition->id }}" selected>
-                                            {{$condition->condition}}
-                                        </option>
-                                    @endforeach
-                            </select>
+        @if ($type === '2')
+        <div class="form-group pt-4">
+            <label for="buy_now_price">Buy now price:</label><br>
+
+            <input id="buy_now_price"
+            type="number"
+            name="buy_now_price"
+            wire:model="buy_now_price"
+            placeholder="1.0" step="0.01" min="0.1">
+        </div>
+        @endif
+
+        <div class="pt-2">
+            <div class="card">
+                <div class="card-body">
+                    @foreach ($items as $index => $item)
+                        <hr>
+                        <div class="form-group">
+                            <label for="item_title">Item title*</label>
+                            <input type="text"
+                                    name="items[{{$index}}][item_title]"
+                                    class="form-control"
+                                    wire:model="items.{{$index}}.item_title" />
                         </div>
-                    </div>
-                    <div class="form-group pt-4">
-                        <label for="price">Price*:</label><br>
+                        <div class="form-group pt-4">
+                            <input type="file"
+                                    name="items[{{$index}}][image]"
+                                    placeholder="Choose image"
+                                    id="image"
+                                    wire:model="items.{{$index}}.image" />
+                        </div>
+                        <div class="form-group pt-4">
+                            <div class="col-md-2">
+                                <label>Condition*</label>
+                                <select class="form-control"
+                                        name="items[{{$index}}][condition]"
+                                        wire:model="items.{{$index}}.condition"
+                                        required>
+                                        @foreach($conditions as $condition)
+                                            <option value="{{ $condition->id }}" selected>
+                                                {{$condition->condition}}
+                                            </option>
+                                        @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group pt-4">
+                            <label for="price">
+                                @if ($type === '1')
+                                    Price*:
+                                @else
+                                    Starting price*:
+                                @endif
+                            </label><br>
                         <input id="price"
                                 type="number"
                                 name="items[{{$index}}][price]"
                                 wire:model="items.{{$index}}.price"
                                 placeholder="1.0" step="0.01" min="0.1" class="@error('price') is-invalid @enderror" required>
+                        </div>
+
+                        @if ($type === '1')
+                            <div class="form-group pt-4">
+                                <label for="quantity">Quantity*:</label><br>
+                                <input id="quantity"
+                                        type="number"
+                                        name="items[{{$index}}][quantity]"
+                                        wire:model="items.{{$index}}.quantity"
+                                        placeholder="1" step="1" min="1" class="@error('quantity') is-invalid @enderror" required>
+                            </div>
+                        @else
+                            <div class="form-group pt-4">
+                                <label for="reserve_price">Reserve price:</label><br>
+
+                                <input id="reserve_price"
+                                type="number"
+                                name="items[{{$index}}][reserve_price]"
+                                wire:model="items.{{$index}}.reserve_price"
+                                placeholder="1.0" step="0.01" min="0.1">
+                            </div>
+                        @endif
+                        
+                        <div class="form-group pt-4">
+                            <button class="btn btn-sm btn-danger"
+                                wire:click.prevent="removeItem({{$index}})">- Remove Item</button>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <button class="btn btn-sm btn-secondary"
+                            wire:click.prevent="addItem">+ Add Item</button>
                     </div>
-                    <div class="form-group pt-4">
-                        <button class="btn btn-sm btn-danger"
-                            wire:click.prevent="removeItem({{$index}})">- Remove Item</button>
-                    </div>
-                @endforeach
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <button class="btn btn-sm btn-secondary"
-                        wire:click.prevent="addItem">+ Add Item</button>
                 </div>
             </div>
         </div>
