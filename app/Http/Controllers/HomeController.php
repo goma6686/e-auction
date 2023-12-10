ed732608042979cfc2ba9e94fe40544dd081d0d5
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-12-10 21:43:10 +0200

    Refactor HomeController methods and add new functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Auction;
+use App\Models\Item;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -13,17 +14,23 @@ class HomeController extends Controller
     {
         $categories = Category::all();
 
-        $auction_items = Auction::where('is_active', true)
-            ->where('end_time', '>', now())
+        $auction_items = Auction::withCount(['bids', 'items'])
+            ->where('is_active', true)
             ->with(['items', 'category', 'items.condition'])
-            ->select(
-                '*',
-            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as max_price'),
-            DB::raw('(SELECT MIN(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as min_price'),
-            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as price'),
-            DB::raw('(SELECT COUNT(*) FROM items WHERE items.auction_uuid = auctions.uuid) as count')
-            )
-            ->orderByDesc('auctions.bidder_count')
+            ->addSelect([
+                'max_price' => Item::selectRaw('MAX(price)')
+                    ->whereColumn('auction_uuid', 'auctions.uuid')
+                    ->havingRaw('COUNT(*) > 1'),
+                'min_price' => Item::selectRaw('MIN(price)')
+                    ->whereColumn('auction_uuid', 'auctions.uuid')
+                    ->havingRaw('COUNT(*) > 1'),
+                'buy_price' => Item::selectRaw('MAX(price)')
+                    ->whereColumn('auction_uuid', 'auctions.uuid')
+                    ->havingRaw('COUNT(*) = 1'),
+                'count' => Item::selectRaw('COUNT(*)')
+                    ->whereColumn('auction_uuid', 'auctions.uuid'),
+            ])
+            ->orderByDesc('bids_count')
             ->groupBy('auctions.category_id', 'auctions.uuid')
             ->take(3)
             ->get();
@@ -40,12 +47,50 @@ class HomeController extends Controller
                 '*',
             DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as max_price'),
             DB::raw('(SELECT MIN(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as min_price'),
-            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as price'),
+            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as buy_price'),
             DB::raw('(SELECT COUNT(*) FROM items WHERE items.auction_uuid = auctions.uuid) as count')
             )
             ->orderByDesc('auctions.created_at')
             ->paginate(10);
 
+        return view ('home', compact('categories', 'auctions'));
+    }
+
+    public function auctions(){
+        $categories = Category::all();
+
+        $auctions = Auction::where('is_active', true)
+            ->where('type_id', '=', "2")
+            ->with(['items', 'category', 'items.condition'])
+            ->select(
+                '*',
+            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as max_price'),
+            DB::raw('(SELECT MIN(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as min_price'),
+            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as buy_price'),
+            DB::raw('(SELECT COUNT(*) FROM items WHERE items.auction_uuid = auctions.uuid) as count')
+            )
+            ->orderByDesc('auctions.created_at')
+            ->paginate(10);
+
+        return view ('home', compact('categories', 'auctions'));
+    }
+
+    public function buy(){
+        $categories = Category::all();
+
+        $auctions = Auction::where('is_active', true)
+            ->where('type_id', '=', "1")
+            ->with(['items', 'category', 'items.condition'])
+            ->select(
+                '*',
+            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as max_price'),
+            DB::raw('(SELECT MIN(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as min_price'),
+            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as buy_price'),
+            DB::raw('(SELECT COUNT(*) FROM items WHERE items.auction_uuid = auctions.uuid) as count')
+            )
+            ->orderByDesc('auctions.created_at')
+            ->paginate(10);
+            //return dd($auctions);
         return view ('home', compact('categories', 'auctions'));
     }
 
@@ -57,16 +102,17 @@ class HomeController extends Controller
         else {
             $auctions = Auction::where('is_active', true)
             ->where('category_id', Category::where('category', $category)->first()->id)
-            ->with(['items', 'category'])
+            ->with(['items', 'category', 'items.condition'])
             ->select(
                 '*',
             DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as max_price'),
             DB::raw('(SELECT MIN(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) > 1) as min_price'),
-            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as price'),
+            DB::raw('(SELECT MAX(price) FROM items WHERE items.auction_uuid = auctions.uuid HAVING COUNT(*) = 1) as buy_price'),
             DB::raw('(SELECT COUNT(*) FROM items WHERE items.auction_uuid = auctions.uuid) as count')
             )
             ->paginate(10);
 
+            //return dd($auctions);
             return view ('home', compact('categories', 'auctions'));
         }
         
