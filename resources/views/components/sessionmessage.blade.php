e8812f23cda4e2b19f4f92ee7a4a4757a30b4887
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-12-18 13:01:35 +0200

    flash message timeout with alpine js

--- resources/views/components/sessionmessage.blade.php
+++ resources/views/components/sessionmessage.blade.php
@@ -1,14 +1,16 @@
-@if(Session::has('error'))
-        <div class="d-flex alert alert-danger">
-            <ul class="mx-auto justify-content-center">
-                <li>{{ session()->get('error') }}</li>
-            </ul>
-        </div>
+<div x-data="{show: true}" x-init="setTimeout(() => show = false, 5000)" x-show="show">
+    @if(Session::has('error'))
+    <div class="d-flex alert alert-danger">
+        <ul class="mx-auto justify-content-center">
+            <li>{{ session()->get('error') }}</li>
+        </ul>
+    </div>
     @endif
     @if(Session::has('success'))
-        <div class="d-flex alert alert-success">
-            <ul class="mx-auto justify-content-center">
-                <li>{{ session()->get('success') }}</li>
-            </ul>
-        </div>
+    <div class="d-flex alert alert-success">
+        <ul class="mx-auto justify-content-center">
+            <li>{{ session()->get('success') }}</li>
+        </ul>
+    </div>
     @endif
+</div>
