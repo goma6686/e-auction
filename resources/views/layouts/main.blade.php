0b548a4df21d338234cc2ad792af47b70f24b8b1
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-12-31 13:44:53 +0200

    fixed welcome css + admin.css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,13 +7,22 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     @include('layouts.header')
-    <main id="root" class="row">
-        <div class="sidebar col-1">
-            @yield('sidebar')
-        </div>
-        <div class="content col-9">
-            @yield('content')
-        </div>
+        @if (Route::currentRouteName() === 'welcome')
+        <main id="root" class="row">
+            <div class="sidebar col-1">
+                @yield('sidebar')
+            </div>
+            <div class="content col-9">
+                @yield('content')
+            </div>
+        </main>
+        @else
+        <main class="container pt-3">
+            <div class="content">
+                @yield('content')
+            </div>
+        </main>
+        @endif
     </main>
     @livewireScripts
     @vite(['resources/js/app.js'])
